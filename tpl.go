46efd8ee329536f3b2d95e9d8c93788721024f34
kafei <229482191@qq.com>
2022-01-19 16:21:34 +0800

    support new from sync.pool

---
+++ tpl.go
@@ -0,0 +1,64 @@
+package main
+
+const tplFactoryOutline = `{comment}
+package {package}
+
+type factory func() interface{}
+
+{spawnerFunc}
+
+var spawner = map[string]factory{
+	{mapContext}
+}
+
+{poolPut}
+`
+
+const tplPoolOutline = `{comment}
+package {package}
+
+
+import "sync"
+
+var spawnerPools = map[string]*sync.Pool{
+	{poolObject}
+}
+`
+
+const tplSpawner = `
+func Spawner(name string, newPool ...bool) (interface{},bool) {
+	f ,ok := spawner[name]
+	if !ok{
+		return nil,ok
+	}
+	return f(),true
+}`
+
+const tplSpawnerPool = `
+func Spawner(name string, newPool ...bool) (interface{}, bool) {
+	if len(newPool) > 0 && newPool[0] {
+		return spawnerPools[name].Get(),true
+	}
+	f, ok := spawner[name]
+	if !ok {
+		return nil, ok
+	}
+	return f(), true
+}`
+
+const tplPoolPut = `
+func Put(name string, x interface{}){
+	pool, ok := spawnerPools[name]
+	if !ok {
+		return
+	}
+	pool.Put(x)
+}`
+const tplWithoutPoolPut = `
+func Put(name string, x interface{}){}`
+
+const tplMapField = `"{package}.{name}":func() interface{} { return &{name}{} },
+`
+
+const tplMapPoolField = `"{package}.{name}":{New: func() interface{}{ return spawner["{package}.{name}"]}},
+`
